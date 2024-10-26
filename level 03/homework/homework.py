c8ea0e3524bcb11c2d911b5b422884240ab3a0c1
giorgi685 <gnebieridze83@gmail.com>
2024-10-26 21:31:52 +0400

    goa

---
+++ level 03/homework/homework.py
@@ -0,0 +1,67 @@
+#3) შექმენი ცვლადები,  სახელი,გვარი,ასაკი,ჰობი,ქვეყანა და  ეს ყველა ცვლადი დაპრინტეთ. 
+name = "giorgi"
+surname = "nebieridze"
+age = "12"
+hobby = "watch film"
+country = "georgia"
+
+#4) შექმენით ცვლადები, გადაეცით მნიშვნელობები და შემდგომ დაამატეთ ერთმანეთზე.
+name = "giorgi"
+city = "kutaisi"
+favorite_pets = "dog"
+favorite_food = "khinkali"
+favorite_sport = "football"
+heigh = 1.48
+print(name + " " + city + " " + favorite_pets + " " + favorite_food + " " +  favorite_sport + " " + str(heigh))
+
+
+#5)  გააკეთეთ 10 მაგალითი ყველა მათემათიკურ ოპერატორებზე. +,-,*,//,**
+print(5+9)
+print(8+4)
+print(7+5)
+print(3+7)
+print(12+31)
+print(32+15)
+print(62+34)
+print(76+32)
+print(71+41)
+print(90+13)
+
+
+print(28-13)
+print(15-7)
+print(20-12)
+print(89-84)
+print(43-21)
+print(90-89)
+print(74-29)
+print(78-38)
+print(92-83)
+print(36-26)
+
+
+print(5*6)
+print(8*3)
+print(13*4)
+print(32*2)
+print(13*4)
+print(7*11)
+print(31*4)
+print(54*3)
+print(52*6)
+print(15*6)
+
+
+print(15//5)
+print(30//3)
+print(70//2)
+print(35//7)
+print(90//15)
+print(36//4)
+print(14//7)
+print(81//9)
+print(21//3)
+print(32//8)
+
+
+
