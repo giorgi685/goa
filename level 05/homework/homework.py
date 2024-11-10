f5de3fb40d641ca127bff849d4ee8869973a9076
giorgi685 <gnebieridze83@gmail.com>
2024-11-10 13:37:44 +0400

    goa

---
+++ level 05/homework/homework.py
@@ -0,0 +1,15 @@
+# 1
+current_year = 2024
+print("The current year is " + str(current_year))
+
+# 2
+country = "USA"
+print("You are from " + str(country))
+
+# 3
+height = 180
+print("Your height in cm is " + str(height / 180) + " meters")
+
+# 4
+new_name = "Alex"
+print("Hello, " + new_name)
