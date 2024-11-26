50427d2d3252468e650ad1908eb110db8fbc0014
giorgi685 <gnebieridze83@gmail.com>
2024-11-26 21:11:53 +0400

    goa

---
+++ level 07/classwork/classwork.py
@@ -0,0 +1,10 @@
+name = input("შეიყვანე შენი სახელი")
+password = input("შეიყვანე შენი პაროლი")
+print(name,password)
+
+num1 = 24
+num2 = 12
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
