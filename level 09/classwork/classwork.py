59115df15569546ed7c5ba46023d9df1450281e3
giorgi685 <gnebieridze83@gmail.com>
2024-12-15 15:32:17 +0400

    goa

---
+++ level 09/classwork/classwork.py
@@ -0,0 +1,13 @@
+num1 = float(input("შეიყვანე შენი წლოვანება"))
+
+num2 = float(input("შეიყვანე შენი დაბადების რიცხვი"))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2) 
+
+# კონკატენაცია_ კონკატენაცია არის ორი ან მეტი string_ის შეერთება
+
+#int_int_ს გამოაქვს მთელი რიცხვები
+#float_float_ს გამოაქვს ათწილადი რიცხვები 
