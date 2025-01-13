d86b746a889e087331143795c44a23d74068f3c2
giorgi685 <gnebieridze83@gmail.com>
2025-01-13 21:11:23 +0400

    goa

---
+++ level 14/classwork/classwork.py
@@ -0,0 +1,17 @@
+# 1.დაწერეთ პროგრამა რომელიც თუ მოქალაქე არის 18 წლის და მას აქვს მართვის მოწმობა შეუშვებს ქვეყანაში
+
+"aris 18 wlis = true"
+"aqvs martvis mowmoba = true"
+print("aris 18 wlis = true" and "aqvs martvis mowmoba = true")
+
+# 2) მომხმარებელმა ფასდაკლება უნდა მიიღოს თუ 100 ლარზე მეტ რამეს ყიდულობს ან არის vip მომხმარებელი
+
+"unda miigos pasdakleba = true"
+"aris vip momxmarebeli = false"
+print("unda miigos pasdakleba = true" or "aris vip momxmarebeli = false")
+
+# 3) ოთახის გათბობა აქტიურდება თუ ტემპერატურა ან ძალიან დაბალია ან ძალიან მაღალი
+
+"dzalian dabalia = false"
+"dzalian magalia = true"
+print("dzalian dabalia = false" or "dzalian magalia = true")
