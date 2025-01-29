c7fdcd1317318ddea550770b0b1e7136dfae8f73
giorgi685 <gnebieridze83@gmail.com>
2025-01-29 21:25:55 +0400

    goa

---
+++ level 16/classwork/classwork.py
@@ -0,0 +1,17 @@
+# 1) მომხმარებელს შემოატანინეთ რიცხვი და შემდეგ ეს რიცხვი გაამრავლეთ 5-ზე და დაპრინტეთ.
+a = int(input("enter your age"))
+print(a*5)
+
+# 2) გამოიყენეთ ლოგიკური ოპერატორები სადაც ამოწმებთ 2 ცალ პირობას. (and/or)
+aris_20_wlis=True
+aris_studenti=True
+print(aris_20_wlis and aris_studenti)
+
+aqvs_martvis_mowmoba=False
+yavs_manqana=True
+print(aqvs_martvis_mowmoba or yavs_manqana)
+
+# 3) შექმენით ცვლადი და შემდეგ დააბდეითეთ ის ახალი მნიშვნელობით.
+
+
+
