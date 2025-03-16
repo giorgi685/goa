68527ae15d23aca1b50721a2790ec68adbac98ec
giorgi685 <gnebieridze83@gmail.com>
2025-03-16 14:29:20 +0400

    goa

---
+++ level 22/classwork/classwork.py
@@ -0,0 +1,20 @@
+for i in range(2,101, 2):
+    print(i)
+
+for i in range(1,101,2):
+    print(i)
+
+age=0
+for i in range(1,101):
+    age = age + i
+    print(age)
+
+age=0    
+for i in range(2,101,2):
+    age = age + i
+    print(age)
+
+age=0
+for i in range(1,101,2):
+    age = age + i
+    print(age)
